174834bb38309560bdd7d0240b090b26889cb251
yehaitao-web <ye-haitao@qq.com>
2019-12-16 13:43:58 +0800

    添加日志配置

--- src/main/java/com/wp/controller/CategoryController.java
+++ src/main/java/com/wp/controller/CategoryController.java
@@ -2,6 +2,8 @@ package com.wp.controller;
 import java.util.List;
 
 import com.wp.pojo.Category;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,11 +14,11 @@ import com.wp.mapper.CategoryMapper;
 @Controller
 public class CategoryController {
     @Autowired CategoryMapper categoryMapper;
-     
+    private static final Logger log = LoggerFactory.getLogger(CategoryController.class);
     @RequestMapping("/listCategory")
     public String listCategory(Model m) throws Exception {
         List<Category> cs=categoryMapper.findAll();
-         
+         log.info("123");
         m.addAttribute("cs", cs);
          
         return "listCategory";
